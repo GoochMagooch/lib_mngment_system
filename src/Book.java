1517ef90a4a15a0105e319005e5d6fe62c838250
Magooch <crvallecito@gmail.com>
2024-10-23 14:00:37 -0600

    Add book objects

--- src/Book.java
+++ src/Book.java
@@ -14,4 +14,13 @@ public class Book {
     public void bookCheckOut() {
         isCheckedOut = true;
     }
+
+    public static void main(String[] args) {
+        Book sleepTight = new Book("Sleep Tight", "J.H Markhert", 0, false);
+        Book salemLot = new Book("Salem's Lot", "Stephen King", 1, false);
+        Book companyOfOne = new Book("Company of One", "Paul Jarvis", 2, false);
+        Book mindset = new Book("Mindset", "Carol Dweck", 3, false);
+        Book quiet = new Book("Quiet", "Susan Cain", 4, false);
+    }
+
 }
